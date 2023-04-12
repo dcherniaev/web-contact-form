9cafe01ce6e272909c7c67fd722b1e900cb7978e
Dmitrii <dmitryc174@gmail.com>
2023-04-12 21:48:19 +0400

    MVP without extra challenge from task

--- check.php
+++ check.php
@@ -1,1 +1,29 @@
 <?php
+
+$issue = $_POST['issue'];
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+?>
+<html>
+<head>
+    <title>check input data</title>
+</head>
+<body>
+<table>
+    <tr>
+        <td>Issue:</td><td><?php echo $issue ?></td>
+    </tr>
+    <tr>
+        <td>Name:</td><td><?php echo $name ?></td>
+    </tr>
+    <tr>
+        <td>Email:</td><td><?php echo $email ?></td>
+    </tr>
+    <tr>
+        <td>Message:</td><td><?php echo $message ?></td>
+    </tr>
+</table>
+</body>
+</html>
